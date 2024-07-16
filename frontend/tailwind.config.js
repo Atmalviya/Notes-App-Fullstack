5c67b167249fb2797b9f84b4dc4e085deae4c0f3
Atul <atulmalviyav2@gmail.com>
2024-07-16 12:48:13 +0530

    Boiler plate code and tailwind css configuration

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      // color used in project
+      colors: {
+        primary: "#2B85FF",
+        secondary: "#EF863E",
+      },
+    },
+    plugins: [],
+  },
+};
