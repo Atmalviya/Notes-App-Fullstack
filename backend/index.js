ffcdb3125f3de596db2572090d7abe6d1cfa1b7c
Atul <atulmalviyav2@gmail.com>
2024-07-16 12:39:53 +0530

    main Index.js file

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const connecteDB = require("./db.js");
+
+const userRoutes = require("./routes/user.routes");
+const noteRoutes = require("./routes/note.routes");
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+    credentials: true,
+  })
+);
+
+//* mongodb connection
+connecteDB();
+
+app.get("/", (req, res) => {
+  res.json({ data: "hello" });
+});
+
+app.use("/user", userRoutes);
+app.use("/note", noteRoutes);
+
+//* start server
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
+
+module.exports = app;
